5a647a5187aa9b8ad357d9c4837c49478a251213
pasahgroup <109248164+pasahgroup@users.noreply.github.com>
2023-10-19 13:55:22 +0300

    19 Oct 2023

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -58,8 +58,9 @@ License: You must have a valid license purchased only from themeforest(the above
   });
 }
     </script>
+    <script src="../../js/jQuery311.min.js" type="text/javascript"></script>
 
-	<link rel="shortcut icon" href="../../assets/images/misc/logo.svg" />
+<link rel="shortcut icon" href="../../assets/images/misc/logo.svg" />
 </head>
 
 <body id="tc_body" class="header-fixed header-mobile-fixed subheader-enabled aside-enabled aside-fixed">
@@ -94,7 +95,7 @@ License: You must have a valid license purchased only from themeforest(the above
          @endrole
 			<button class="btn p-0" id="tc_aside_mobile_toggle">
 				<svg width="20px" height="20px" viewBox="0 0 16 16" class="bi bi-justify-right" fill="currentColor"
-					xmlns="http://www.w3.org/2000/svg">
+					xmlns="#">
 					<path fill-rule="evenodd"
 						d="M6 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-4-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z" />
 				</svg>
@@ -103,7 +104,7 @@ License: You must have a valid license purchased only from themeforest(the above
 			<button class="btn p-0 ml-2" id="tc_header_mobile_topbar_toggle">
 				<span class="svg-icon svg-icon-xl">
 					<svg width="20px" height="20px" viewBox="0 0 16 16" class="bi bi-person-fill" fill="currentColor"
-						xmlns="http://www.w3.org/2000/svg">
+						xmlns="#">
 						<path fill-rule="evenodd"
 							d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
 					</svg>
@@ -118,8 +119,15 @@ License: You must have a valid license purchased only from themeforest(the above
 	<div class="d-flex flex-column flex-root"   style="background-color:#fff !important">
 		<!--begin::Page-->
 		<div class="d-flex flex-row flex-column-fluid page"  style="background-color:#fff !important">
-			<!--begin::Aside-->
-		<div>
+
+      <!--begin::Aside-->
+  @role('Manager|Admin|GeneralManager|GeneralAdmin|SuperAdmin|User|Store|HouseKeeper|Maintenancier')
+
+  <?php
+  $wrapper="wrapper";
+   ?>
+
+       <div>
 			<div class="aside aside-left aside-fixed d-flex flex-column flex-row-auto" id="tc_aside">
 				<!--begin::Brand-->
 				<div class="brand flex-column-auto" id="tc_brand" style="background-color:#">
@@ -134,7 +142,7 @@ License: You must have a valid license purchased only from themeforest(the above
 <!-- <span class="brand-text"><img style="height: 60px;width: 150px;" alt="Logo" src="{{ asset('storage/logo/'.$company->logo)}}"/></span> -->
 @endisset
 	</a>
-				</div>
+       </div>
 
 				<!--end::Brand-->
 				<!--begin::Aside Menu-->
@@ -150,10 +158,12 @@ License: You must have a valid license purchased only from themeforest(the above
 				<!--end::Aside Menu-->
 			</div>
 		</div>
+@endrole
+
 			<div class="aside-overlay"></div>
 			<!--end::Aside-->
 			<!--begin::Wrapper-->
-			<div class="d-flex flex-column flex-row-fluid wrapper" id="tc_wrapper">
+			<div class='d-flex flex-column flex-row-fluid @isset($wrapper)<?php echo $wrapper ?>@endisset' id="tc_wrapper">
 				<!--begin::Header-->
 				<div id="tc_header" class="header header-fixed" style="background-color: #">
 					<!--begin::Container-->
@@ -172,7 +182,7 @@ License: You must have a valid license purchased only from themeforest(the above
 											<span class="svg-icon svg-icon-xl svg-icon-primary">
 
 												<svg width="24px" height="24px" viewBox="0 0 16 16" class="bi bi-list"
-													fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+													fill="currentColor" xmlns="#">
 													<path fill-rule="evenodd"
 														d="M2.5 11.5A.5.5 0 0 1 3 11h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 7h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4A.5.5 0 0 1 3 3h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
 												</svg>
@@ -221,12 +231,13 @@ License: You must have a valid license purchased only from themeforest(the above
 
 							<!--begin::Notifications-->
 							<div class="dropdown">
+                @role('SuperAdmin|GeneralAdmin|GeneralManager|Manager|Admin|HouseKeeping|Maintenancier')
 								<div class="topbar-item" data-toggle="dropdown" data-display="static">
 									<div class="btn btn-icon btn-clean btn-dropdown mr-1">
 										<div class="svg-icon svg-icon-xl svg-icon-primary" title="Notification">
 
 											<svg width="20px" height="20px" viewBox="0 0 16 16" class="bi bi-bell"
-												fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+												fill="currentColor" xmlns="#">
 												<path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2z" />
 												<path fill-rule="evenodd"
 													d="M8 1.918l-.797.161A4.002 4.002 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4.002 4.002 0 0 0-3.203-3.92L8 1.917zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5.002 5.002 0 0 1 13 6c0 .88.32 4.2 1.22 6z" />
@@ -237,15 +248,16 @@ License: You must have a valid license purchased only from themeforest(the above
 											</div>
 										</div>
 
-										<span class="badge badge-pill badge-primary">0</span>
+										<span class="badge badge-pill badge-secondary">{{$qnsCount->count() ?? 0}}</span>
 									</div>
 								</div>
+@endrole
 
 								<div class="dropdown-menu p-0 m-0 dropdown-menu-right w-300px">
 									<form>
 
 										<div class="d-flex flex-column p-3 border-bottom rounded-top">
-
+ {{--
 											<h4
 												class="d-flex justify-content-between align-items-center mb-0 rounded-top">
 												<span class="font-size-h5 ">Notifications</span>
@@ -253,30 +265,30 @@ License: You must have a valid license purchased only from themeforest(the above
 													Clear
 												</a>
 											</h4>
-
+ --}}
 										</div>
 
                                         <div class="nav-hover scrollbar-1 ">
 										 {{-- <div class="nav nav-hover scrollbar-1 "> --}}
-                                              <p class="text-center">No notification</p>
-                                                        {{--
-											<a href="#" class="nav-item border-bottom">
+                                              <!-- <p class="text-center">No notification</p> -->
+
+											<a href="/managers-inspection" class="nav-item border-bottom">
 												<div class="nav-link">
 													<div class="nav-icon mr-3">
-														<i class="fas fa-cog text-primary"></i>
+														<!-- <i class="fas fa-cog text-primary"></i> -->
 													</div>
 													<div class="nav-text">
-														<div class="font-weight-bold">New report has been received</div>
-														<div class="text-muted">23 hrs ago</div>
+														<div class="font-weight-bold" style="color:red">Urgently We need to clear all addressed Issues</div>
+														<!-- <div class="text-muted">23 hrs ago</div> -->
 													</div>
 												</div>
-											</a>--}}
+											</a>
 
                                         </div>
 										<div class="d-flex flex-column p-3 rounded-top">
 
 											<h4 class="d-flex justify-content-center mb-0  rounded-top">
-												<a href="#" class="font-size-base text-primary">View All</a>
+												<a href="/managers-inspection" class="font-size-base text-primary">View All</a>
 											</h4>
 
 										</div>
@@ -289,7 +301,6 @@ License: You must have a valid license purchased only from themeforest(the above
 
 							<!--begin::user-->
 							<div class="dropdown">
-
 								<div class="topbar-item" data-toggle="dropdown" data-display="static">
 									<div class="btn btn-icon w-auto btn-clean d-flex align-items-center pr-1 pl-3">
 										<span class="text-dark-50 font-size-base d-none d-xl-inline mr-3">@auth{{ Auth::user()->name }}@endauth</span>
@@ -297,7 +308,7 @@ License: You must have a valid license purchased only from themeforest(the above
 											<span class="symbol-label font-size-h5 ">
 												<svg width="20px" height="20px" viewBox="0 0 16 16"
 													class="bi bi-person-fill" fill="currentColor"
-													xmlns="http://www.w3.org/2000/svg">
+													xmlns="#">
 													<path fill-rule="evenodd"
 														d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
 												</svg>
@@ -310,7 +321,7 @@ License: You must have a valid license purchased only from themeforest(the above
 
 									<a href="#" class="dropdown-item">
 										<span class="svg-icon svg-icon-xl svg-icon-primary mr-2">
-											<svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px"
+											<svg xmlns="#" width="20px" height="20px"
 												viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
 												stroke-linecap="round" stroke-linejoin="round"
 												class="feather feather-user">
@@ -325,7 +336,7 @@ License: You must have a valid license purchased only from themeforest(the above
 								onclick="event.preventDefault();
 											  document.getElementById('logout-form').submit();">
 										<span class="svg-icon svg-icon-xl svg-icon-primary mr-2">
-											<svg xmlns="http://www.w3.org/2000/svg" width="20px" height="20px"
+											<svg xmlns="#" width="20px" height="20px"
 												viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
 												stroke-linecap="round" stroke-linejoin="round"
 												class="feather feather-power">
@@ -408,8 +419,7 @@ License: You must have a valid license purchased only from themeforest(the above
 	</div>
 	<!--end::Main-->
 
-	<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script> -->
-	<script src="../../../assets/js/plugin.bundle.min.js"></script>
+  <script src="../../../assets/js/plugin.bundle.min.js"></script>
 	<script src="../../../assets/js/bootstrap.bundle.min.js"></script>
 	<script src="../../../assets/api/jqueryvalidate/jquery.validate.min.js"></script>
 	<script src="../../../assets/api/apexcharts/apexcharts.js"></script>

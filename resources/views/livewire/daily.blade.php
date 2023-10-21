4121ba362968a86866a4dcb22ce1f153965b6833
pasahgroup <109248164+pasahgroup@users.noreply.github.com>
2023-10-21 09:27:30 +0300

    21 oct 2023

--- resources/views/livewire/daily.blade.php
+++ resources/views/livewire/daily.blade.php
@@ -54,7 +54,7 @@ border-color: #dddddd;
 }
 </style>
 
-<script type="text/javascript" src="../js/activitydata.js"></script>
+<!-- <script type="text/javascript" src="../js/activitydata.js"></script> -->
 <script type="text/javascript" src="../js/jquery.js"></script>
 <script type="text/javascript" src="../js/jquery311.min.js"></script>
 
@@ -609,5 +609,47 @@ for (var i = 0; i < radios.length; i++) {
      }
      </script>
 
- 
+     <script type="text/javascript">
+       $(document).ready(function(){
+
+      // Department Change
+      $('#metaname_model').change(function(){
+         // ward
+         var v = $(this).val();
+           // Empty the dropdown
+          $('#asset_model').find('option').not(':first').remove();
+         // $('#village').find('option').not(':first').remove();
+         // $('#project_name').find('option').not(':first').remove();
+         // $('#project_activities').find('option').not(':first').remove();
+
+//alert(v);
+         // AJAX request
+         $.ajax({
+           url: 'getA/'+v,
+           type: 'get',
+           dataType: 'json',
+           success: function(response){
+
+             var len = 0;
+             if(response['dataA'] != null){
+               len = response['dataA'].length;
+             }
+
+                       if(len > 0){
+               // Read data and create <option >
+               for(var i=0; i<len; i++){
+
+                 var id = response['dataA'][i].id;
+                 var name = response['dataA'][i].asset_name;
+                 var option = "<option value='"+id+"'>"+name+"</option>";
+                 $("#asset_model").append(option);
+               }
+             }
+             //DAta are here
+
+           }
+        });
+      });
+    });
+     </script>
 @endsection
